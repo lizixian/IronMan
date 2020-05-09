5f06bfd84db80fb2e0bc16298fb68033e1cab336
lizixian <lizixian@yy.com>
2020-05-09 17:20:51 +0800

    create module

--- app/src/main/java/com/avengers/ironman/MainActivity.kt
+++ app/src/main/java/com/avengers/ironman/MainActivity.kt
@@ -5,6 +5,22 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * app信息获取
+     * 手机信息获取
+     * 大图检测
+     * ANR检测
+     * OOM检测
+     * 函数耗时统计
+     * 启动耗时统计
+     * 流量监控
+     * 耗电监控
+     * FPS
+     * CPU使用率
+     * 内存占用
+     * 页面加载时间
+     * catch统计
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
